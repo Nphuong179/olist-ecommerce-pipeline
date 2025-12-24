7f744c3a64ce91525ed099a8a905ecba6651c54a
unknown <nphuong.nguyen281@gmail.com>
2025-12-25 00:50:35 +0700

    developed fact_order_payments model

---
+++ olist_dbt/models/facts/fact_order_payments.sql
@@ -0,0 +1,56 @@
+{{ config(materialized='table') }}
+
+WITH order_payment_aggregates AS (
+    SELECT
+        order_id,
+        COUNT(*) AS payment_transaction_count,
+        SUM(payment_value) AS total_payment,
+        MAX(payment_value) AS largest_payment
+    FROM {{ ref("stg_order_payments") }}
+    GROUP BY order_id
+)
+
+SELECT
+    -- Natural key
+    sop.order_id,
+    sop.payment_transaction_number,
+
+    -- Payment attributes
+    sop.payment_type,
+    sop.payment_installments,
+
+    -- Monetary values
+    sop.amount,
+
+    CASE
+        WHEN opa.total_payment > 0
+        THEN (sop.amount / opa.total_payment)
+    END AS payment_share_of_order,
+
+    CASE
+        WHEN sop.payment_installments > 0
+        THEN (sop.amount / sop.payment_installments)
+    END AS monthly_installment_amount,
+
+    -- Derived flag: Payment characteristics
+    CASE
+        WHEN sop.amount = opa.largest_payment
+        THEN TRUE
+        ELSE FALSE
+    END AS is_primary_payment,
+
+    CASE
+        WHEN opa.payment_transaction_count = 1
+        THEN TRUE
+        ELSE FALSE
+    END AS is_only_payment,
+
+    CASE
+        WHEN sop.payment_installments > 1
+        THEN TRUE
+        ELSE FALSE
+    END AS is_installment_payment
+
+FROM {{ ref("stg_order_payments") }} sop
+LEFT JOIN order_payments_aggregates opa
+    ON sop.order_id = opa.order_id
