1cfa9a0d6073e01e82b9a8a7b6688cb5f610913b
unknown <nphuong.nguyen281@gmail.com>
2025-12-25 18:55:48 +0700

    update stg_reviews to use timestamp instead of date

--- olist_dbt/models/staging/stg_order_reviews.sql
+++ olist_dbt/models/staging/stg_order_reviews.sql
@@ -6,7 +6,7 @@ SELECT
     "review_score" as review_score,
     trim("review_comment_title") as review_title,
     trim("review_comment_message") as review_message,
-    "review_creation_date"::DATE as review_created_date,
+    "review_creation_date"::TIMESTAMP as review_created_timestamp,
     "review_answer_timestamp"::TIMESTAMP as review_answer_timestamp
 
 FROM {{ source("raw", "raw_order_reviews") }}
