6d239d4badc2f7281c2d5403a3f0c8848ca5c12d
unknown <nphuong.nguyen281@gmail.com>
2026-02-27 13:59:51 +0700

    remove print() in load_data

--- olist_dbt/load_data.py
+++ olist_dbt/load_data.py
@@ -7,19 +7,10 @@ def main():
     data_dir = project_root/'data'/'raw'
     db_path = project_root/'olist_dbt'/'dev.duckdb'
 
-    print("Checking paths")
-    print(f"Project root: {project_root}")
-    print(f"Data dir: {data_dir}")
-    print(f"database path: {db_path}")
-    print(f"Data dir exists {data_dir.exists()}")
-    print(f"Data dir is dir {data_dir.is_dir()}")
-
     csv_files = list(data_dir.glob('*.csv'))
-    print(f"Found {len(csv_files)} csv files")
     conn = duckdb.connect(db_path)
     for csv_file in csv_files:
         file_name = csv_file.stem.replace('olist_','raw_').replace('_dataset','')
-        print(f"Loading data from {file_name} into DuckDB")
         conn.execute(f"""
             CREATE OR REPLACE TABLE {file_name} AS
             SELECT * FROM read_csv_auto('{csv_file}')
